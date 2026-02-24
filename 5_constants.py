ae12e06d0ceb3dd6c75d380ee963b69e9a71b6b2
Jed Blenkhorn <hello@jedbillyb.com>
2026-02-25 09:23:11 +1300

    Updated constants

--- 5_constants.py
+++ 5_constants.py
@@ -13,8 +13,8 @@
 # gst_inclusive = round(cost_price*GST_RATE)
 # print("The GST exclusive price is", cost_price, "when you add GST it costs", gst_inclusive)
 
-## This program calculates swimming pool deminsions
-# print("This program calculates swimming pool deminsions")
-# length = int(input("What is the length of the pool in metres? "))
-# width = round(length*0.4)
-# print(f"The width of the pool is {width} metres, and the length is {length} metres")
+# This program calculates swimming pool deminsions
+print("This program calculates swimming pool deminsions")
+length = int(input("What is the length of the pool in metres? "))
+width = round(length*0.4)
+print(f"The width of the pool is {width} metres, and the length is {length} metres")
