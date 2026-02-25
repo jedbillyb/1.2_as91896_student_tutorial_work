98996152c4b8fb49c109e1768aeb36cd5fb2d2f2
Jed Blenkhorn <hello@jedbillyb.com>
2026-02-26 09:55:02 +1300

    Completed adding quit to the gui

--- 6_adding_quit_to_the_gui.py
+++ 6_adding_quit_to_the_gui.py
@@ -1,11 +1,33 @@
+# from tkinter import *
+
+# main_window = None
+
+# def quit():
+#     main_window.destroy()
+
+# def main():
+#     global main_window
+#     main_window = Tk()
+#     Button(main_window, text="Quit", command=quit).grid()
+#     main_window.mainloop()
+
+# main()
+
+
+
 from tkinter import *
+
+main_window = None
 
 def quit():
     main_window.destroy()
 
 def main():
+    global main_window
     main_window = Tk()
-    Button(main_window, text="Quit", command=quit).grid()
+    Button(main_window, text="sortie", command=quit).grid()
+    Button(main_window, text="quit", command=quit).grid()
+    Button(main_window, text="close window", command=quit).grid()
     main_window.mainloop()
 
 main()
